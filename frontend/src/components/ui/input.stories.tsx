0f7a7b0caf64346408165117c23aebabaf766005
Enio Junior <junior-enio@outlook.com>
2026-08-12 18:37:48 -0400

    feat: add input component

---
+++ frontend/src/components/ui/input.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Mail } from "lucide-react";
+
+import { Input } from "./input";
+
+const meta = {
+  title: "UI/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      control: "text",
+    },
+    helperText: {
+      control: "text",
+    },
+    error: {
+      control: "text",
+    },
+    placeholder: {
+      control: "text",
+    },
+    icon: {
+      control: false,
+    },
+  },
+  args: {
+    label: "Label",
+    helperText: "Helper",
+    placeholder: "Placeholder",
+    icon: <Mail />,
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Simple: Story = {
+  args: {
+    helperText: undefined,
+    icon: undefined,
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    defaultValue: "Text",
+  },
+};
+
+export const Error: Story = {
+  args: {
+    defaultValue: "Text",
+    error: "Helper",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    defaultValue: "Text",
+    disabled: true,
+  },
+};
