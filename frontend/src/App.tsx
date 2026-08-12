42449d013bf7dce0e69aec6cff695518236fad70
Enio Junior <junior-enio@outlook.com>
2026-08-12 17:42:06 -0400

    feat: add favicon and logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Button } from "./components/ui/button";
+import Logo from "./assets/logo.svg";
 
 function App() {
   return (
     <>
+      <img src={Logo} alt="Financy logo" />
       <h1 className="text-2xl font-bold">Hello world</h1>{" "}
       <Button variant="outline">Button</Button>
     </>
